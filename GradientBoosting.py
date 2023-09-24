f59c34c37eb0b3eebfe065a7c6684cf37babb0ca
aathi github <aathisnr123@gmail.com>
2023-09-24 21:43:53 +0530

    bug => 114

--- GradientBoosting.py
+++ GradientBoosting.py
@@ -1,21 +1,14 @@
 import pandas as pd
-import xgboost as xgb
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import GradientBoostingClassifier
 
-# Sample input data
-data = [
-    {"text": "Both these are decoctions advised in Jvara (fever) and Kasa (cough).", "medicine": "Punarnavadi Kashaya", "disease": "Fever, Cough"},
-    {"text": "But in a patient with upper respiratory tract infections like common cold (Pinasa), Vyaghryadi would suit better than Punarnavadi.", "medicine": "Vyaghryadi Kashaya", "disease": "Common Cold"},
-    {"text": "Whereas in a condition associated with inflammatory changes all over the body, Punarnavadi would be the appropriate choice.", "medicine": "Punarnavadi Kashaya", "disease": "Inflammatory Conditions"}
-]
-
-# Create a DataFrame from the input data
-df = pd.DataFrame(data)
+# Load your labeled dataset from the CSV file
+data = pd.read_csv("E:/challenge/SIH/machine-learning/DATASET.csv")  # Replace "your_dataset.csv" with the actual path to your CSV file
 
 # Split the data into features (X) and labels (y)
-X = df['text']
-y = df['disease']
+X = data['Symptoms']
+y = data['Name of Medicine']
 
 # Encode the labels into numerical values
 label_encoder = LabelEncoder()
@@ -25,24 +18,43 @@ y_encoded = label_encoder.fit_transform(y)
 vectorizer = TfidfVectorizer()
 X_tfidf = vectorizer.fit_transform(X)
 
-# Create an XGBoost classifier
-model = xgb.XGBClassifier(objective="multi:softmax", num_classes=len(df['disease'].unique()), random_state=42)
+# Create a Gradient Boosting classifier
+model = GradientBoostingClassifier(n_estimators=100, random_state=42)
 
 # Train the model on the TF-IDF vectors and encoded labels
 model.fit(X_tfidf, y_encoded)
 
-# Now, you can use the trained model to predict the disease for new text descriptions of medicines
-new_text = ["common "]
-new_text_tfidf = vectorizer.transform(new_text)
-predicted_disease_encoded = model.predict(new_text_tfidf)
+# Define a function to extract medicines and diseases from a text
+def extract_medicines_and_diseases(text):
+    # Vectorize the input text
+    text_tfidf = vectorizer.transform([text])
+    print(text_tfidf)
+    # Predict the medicine for the input text
+    # print(type(text_tfidf))
+    predicted_medicine_encoded = model.predict(text_tfidf)
+    tem = label_encoder.inverse_transform(text_tfidf)
+    print(tem)
+    
+    print(predicted_medicine_encoded)
+    predicted_medicine = label_encoder.inverse_transform(predicted_medicine_encoded)
+    print(predicted_medicine)
+    # Find the diseases associated with the predicted medicine
+    diseases_with_medicine = data[data['Name of Medicine'] == predicted_medicine[0]]
+    
+    # Extract the list of diseases and their corresponding medicine
+    disease_medicine_list = []
+    for index, row in diseases_with_medicine.iterrows():
+        disease_medicine_list.append({"Disease": row['Symptoms'], "Recommended Medicine": row['Name of Medicine']})
+    
+    return predicted_medicine[0], disease_medicine_list
 
-# Decode the predicted disease back to its original label
-predicted_disease = label_encoder.inverse_transform(predicted_disease_encoded)
+# Provide a new text for extraction
+new_text =  "Ayurveda has a large database of single herbs, minerals, and formulations that have been tailormade to suit each individual, his/her psychosomatic constitution, clinical condition, comorbidities, age, region, etc. These data are spread over more than 150 texts, amidst manuscripts in multiple languages and scripts. With the rise of transcriptional and translational facilities, several traditional medicinal texts are now available in their digitized forms. But for an Ayurvedic student or practitioner, exploring this multitude of literature for identifying their drug of choice' often becomes tedious and impractical. Here is the need of a custom software that can identify the apt formulation that has been designed to treat a constellation of symptoms and present it to the student/practitioner along with its reference and other desired properties. For example, the two formulations Punarnavadi Kashaya and Vyaghryadi Kashaya are mentioned in texts as follows: Both these are decoctions advised in Jvara (fever) and Kasa (cough). But in a patient with upper respiratory tract infections like common cold (Pinasa), Vyaghryadi would suit better than Punarnavadi. Whereas in a condition associated with inflammatory changes all over the body. I Punarnavadi would be the appropriate choice. The objective of the proposed software is to identify the single drugs and formulations that suit a set of symptoms. Certain ingredients (eg. jaggery) are unsuitable for certain categories of patients (e.g. diabetics). There are also medicine mediums that are unsuitable for specific diseases (e.g. fermented/alcoholic preparations in diabetes). Such information is also expected to be conveyed to the learner or practitioner who uses the software. The same disease has been mentioned in different names (E.g. Jvara and Santapa for fever) and the same word has been used to denote different (Eg. Abhaya generally means Terminalia chebula but in the context of Jatyadi ghrita, it means Vetiveria zizanioides. The multiple names of same diseases are expected to be included in the tags of each formulation. The sources for the formulations, and synonyms and similar words have been included in the data section. It is also desirable to include the Ayurvedic pharmacological properties of the single drugs, and the compound formulation (called Rasa, Guna, Virya, Vipaka, etc.) as and where available."
+# Extract the medicine and list of diseases
+medicine, disease_medicine_list = extract_medicines_and_diseases(new_text)
 
-# Create a mapping of medicines to diseases
-medicine_to_disease = dict(zip(df['medicine'], predicted_disease))
-
-# Print the mapping
-print("Medicine to Disease Mapping:")
-for medicine, disease in medicine_to_disease.items():
-    print(f"Medicine: {medicine} => Disease: {disease}")
+# Print the extracted medicine and list of diseases
+print("Extracted Medicine:", medicine)
+print("List of Diseases and Recommended Medicines:")
+for item in disease_medicine_list:
+    print(f"Disease: {item['Disease']}, Recommended Medicine: {item['Recommended Medicine']}")
